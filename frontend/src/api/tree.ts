b2dd9af63c7b0f820db872e19ef7fee410eb0158
NikQ28 <158186802+NikQ28@users.noreply.github.com>
2026-04-04 18:59:09 +0300

    Delete frontend directory

--- frontend/src/api/tree.ts
+++
@@ -1,6 +0,0 @@
-import { api } from './client';
-import type { ItemNode } from '../types';
-
-export const treeApi = {
-  getTree: (rootId: number) => api.get<ItemNode>(`/Tree/${rootId}`),
-};
--- frontend/src/api/watchBoms.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './client';
-import type { WatchBom, WatchBomCreate } from '../types';
-
-export const watchBomsApi = {
-  getAll: () => api.get<WatchBom[]>('/WatchBom'),
-  create: (data: WatchBomCreate) => api.post<number>('/WatchBom', data),
-  update: (id: number, data: WatchBomCreate) => api.put<number>(`/WatchBom/${id}`, data),
-  delete: (id: number) => api.delete<number>(`/WatchBom/${id}`),
-};
