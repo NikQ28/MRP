0522d0b5e8f87124ee6def0f4bf53bb586e3d41a
Nikolay Stebnev <stebnev.nikoly280205@gmail.com>
2026-05-12 02:37:01 +0300

    update calculate orders without negative stocks

--- backend/Domain/Service/CalculateService.cs
+++ backend/Domain/Service/CalculateService.cs
@@ -15,27 +15,28 @@ namespace backend.Domain.Service
                 .Where(o => o.Execution.Date >= datetime.Date)
                 .Where(o => o.Creation.Date <= datetime.Date)
                 .ToList();
+
             List<OrderString> orderStrings = [];
             foreach (var order in orders)
                 orderStrings.AddRange(await orderRepository.GetStringsByOrderIdAsync(order.Id));
             
-            var stock = (await stockRepository.Get())
+            var stocks = (await stockRepository.Get())
                 .Where(s => s.Datetime.Date < datetime.Date)
                 .GroupBy(s => s.ItemId)
-                .ToDictionary(g => g.Key, g => g.Sum(s => s.Operation == OperationType.Coming ? s.Count : -s.Count));
+                .ToDictionary(g => g.Key, g => Math.Max(0, g.Sum(s => s.Operation == OperationType.Coming ? s.Count : -s.Count)));
             var children = boms.GroupBy(b => b.ParentId).ToDictionary(g => g.Key, g => g.ToList());
+            
             List<RequiredItem> required = [];
-           
             foreach (var orderString in orderStrings)
-                CalculateItem(orderString.ItemId, orderString.Count, required, stock, children);
-
+                CalculateItem(orderString.ItemId, orderString.Count, required, stocks, children);
+            
             var materialIds = await treeService.GetMaterialIdsByRootId(1);
             required.RemoveAll(r => !materialIds.Contains(r.ItemId));
-
+            
             return required.OrderBy(r => r.ItemId).ToList();
         }
 
-        private void CalculateItem(int itemId, int requiredCount, List<RequiredItem> required, Dictionary<int, int> stock, Dictionary<int, List<Bom>> children)
+        private void CalculateItem(int itemId, int requiredCount, List<RequiredItem> required, Dictionary<int, int> stocks, Dictionary<int, List<Bom>> children)
         {
             var requiredIds = required.Select(r => r.ItemId).Distinct().ToList();
 
@@ -45,17 +46,17 @@ namespace backend.Domain.Service
             var requiredItem = required.Where(r => r.ItemId == itemId).ToList()[0];
             requiredItem.RequiredCount += requiredCount;
 
-            stock[itemId] = stock.ContainsKey(itemId) ? stock[itemId] : 0;
-            var takeFromStocks = Math.Min(requiredCount, stock[itemId]);
+            stocks[itemId] = stocks.ContainsKey(itemId) ? stocks[itemId] : 0;
+            var takeFromStocks = Math.Min(requiredCount, stocks[itemId]);
 
             requiredItem.StockCount += takeFromStocks;
-            stock[itemId] -= takeFromStocks;
+            stocks[itemId] -= takeFromStocks;
 
             var needToProduce = requiredCount - takeFromStocks;
             requiredItem.NeedCount += needToProduce;
             if (children.TryGetValue(itemId, out var boms))
                 foreach (var bom in boms)
-                    CalculateItem(bom.ComponentId, needToProduce * bom.Count, required, stock, children);
+                    CalculateItem(bom.ComponentId, needToProduce * bom.Count, required, stocks, children);
         }
     }
 }
